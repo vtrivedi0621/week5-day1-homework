e7458b6cebd19022ba378c6b30f502f7f0637f58
Vaishali Trivedi <vaishalitrivedi21@gmail.com>
2022-12-15 13:35:02 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+let mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = 'var(--main-bg)';
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+mainEl.classList.add ('flex-ctr');
+
+let topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = '100%';
+topMenuEl.style.backgroundColor = 'var(--top-menu-bg)';
+topMenuEl.classList.add('flex-around');
+
+for(let i in menuLinks) {
+    let a = document.createElement('a');
+    a.setAttribute("href", menuLinks[i].href)
+    a.textContent = menuLinks[i].text;
+    topMenuEl.append(a);
+}
